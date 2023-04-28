16c8027f709bfa5a713b0374cf0d236b96251c17
minibory <reimex111@gmail.com>
2023-04-28 14:40:34 +0900

    first commit

---
+++ src/main/java/com/mib/platform/customerdataplatform/CustomerDataPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mib.platform.customerdataplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDataPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerDataPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mib/platform/customerdataplatform/CustomerDataPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mib.platform.customerdataplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDataPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
